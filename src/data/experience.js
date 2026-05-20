f52cc8c676ae80905f36f6c97e700f18a391c1d6
Jordan <jg.keightley@gmail.com>
2026-05-20 12:26:40 +0800

    Update content

--- src/data/experience.js
+++ src/data/experience.js
@@ -3,30 +3,30 @@ export const experienceData = [
         title: "Physiotherapist",
         company: "Neuro Logic Phyiotherapy",
         date: "2025 - 2026",
-        description: "Specializing in neurological rehabilitation and navigating NDIS frameworks to deliver tailored, high-quality patient care.",
+        description: "Focusing on neurological rehabilitation and navigating NDIS frameworks to deliver tailored, high-quality patient care.",
         bullets: [
             "Develop comprehensive care plans and funding justifications under NDIS guidelines.",
-            "Implement structural biomechanics and evidence-based conditioning for complex presentations."
+            "Implement evidence-based exercise rehabilitation for complex presentations."
         ]
     },
     {
         title: "Physiotherapist & Specialist Support Coordinator",
         company: "ActiveLINC",
         date: "2022 - 2024",
-        description: "Specializing in neurological rehabilitation and navigating NDIS frameworks to deliver tailored, high-quality patient care.",
+        description: "Focusing on neurological rehabilitation and navigating NDIS frameworks to deliver tailored, high-quality patient care.",
         bullets: [
             "Develop comprehensive care plans and funding justifications under NDIS guidelines.",
-            "Implement structural biomechanics and evidence-based conditioning for complex presentations."
+            "Implement evidence-based exercise rehabilitation for complex presentations."
         ]
     },
     {
         title: "Research Assistant",
         company: "Majarlin",
         date: "2024",
-        description: "Presented at the Annual Scientific Meeting regarding advanced musculoskeletal interventions.",
+        description: "Developed competency of staff in thr area of knowledge synthesis",
         bullets: [
-            "Delivered a comprehensive model of care specifically targeting rotator cuff pain.",
-            "Translated complex research into actionable clinical protocols for spinal cord injury populations."
+            "Guide academics toward the effective completion of knowledge synthesis works.",
+            "Translated best practice guidelines into actionable research project goals."
         ]
     },
     {
@@ -35,6 +35,8 @@ export const experienceData = [
         date: "2022",
         description: "",
         bullets: [
+            "Implement Physiotherapist created treatment programs",
+            "Effective communication between team, patients, and primary Physiotherapist"
         ]
     }
 ];
--- src/data/profile.js
+++ src/data/profile.js
@@ -5,8 +5,8 @@ export const profileData = {
     imageUrl: "https://gravatar.com/avatar/4d9942c0aabcc1f622087a27bbdff67a3148a97a68bbd7023f30132bee11d4bb?v=1733951426000&size=256&d=initials",
     title: "Physiotherapist",
     bio: "Evidence-based practice, exercise rehsbilitation, and a function first approach.",
-    email: "#",
+    githubUrl: "https://github.com/jgkeightley",
     linkedinUrl: "www.linkedin.com/in/jordan-keightley-1b297a3a9", 
-    twitterUrl: "#",  
+    orcidUrl: "https://orcid.org/0000-0003-2753-1154",  
     buttonText: "Save Contact"
 };
